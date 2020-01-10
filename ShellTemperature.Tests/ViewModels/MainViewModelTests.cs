2f42b038a66acaa891d0b016a4327f87a2313f78
Craig Martin <c.martin0297@gmail.com>
2020-01-10 20:26:15 +0000

    Added more comments to files, removed unneeded files

---
+++ ShellTemperature.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using ShellTemperature.ViewModels.ViewModels;
+
+namespace ShellTemperature.Tests.ViewModels
+{
+    public class MainViewModelTests
+    {
+        private readonly MainWindowViewModel _mainWindowViewModel;
+
+        public MainViewModelTests()
+        {
+            _mainWindowViewModel = new MainWindowViewModel();
+        }
+
+        /// <summary>
+        /// Check application version is not null
+        /// </summary>
+        [Test]
+        public void ApplicationVersion_NotNull()
+        {
+            //Assert
+            Assert.IsNotNull(_mainWindowViewModel.ApplicationVersion);
+        }
+
+        /// <summary>
+        /// Check that the application version is the correct format
+        /// </summary>
+        [Test]
+        public void ApplicationVersion_CorrectFormat()
+        {
+            //Arrage
+            char[] appVersionChars = _mainWindowViewModel.ApplicationVersion.ToCharArray();
+            bool isValid = true;
+
+            //Act
+            foreach (char c in appVersionChars)
+            {
+                bool isInt = int.TryParse(c.ToString(), out int i);
+                // not an int, not a dot and not a v
+                if (!isInt && !(c is '.') && !(c is 'V'))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            //Assert
+            Assert.IsTrue(isValid);
+        }
+    }
+}
