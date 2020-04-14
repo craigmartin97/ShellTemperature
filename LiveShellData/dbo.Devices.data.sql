106a18ebb682e12e0577c3bc2c3d7380f61d046d
Craig Martin (L) <c.martin0297@gmail.com>
2020-04-14 20:14:45 +0100

    Added commumentation

--- LiveShellData/dbo.Devices.data.sql
+++ LiveShellData/dbo.Devices.data.sql
@@ -1,2 +1,2 @@
-INSERT INTO [dbo].[Devices] ([Id], [DeviceAddress], [DeviceName]) VALUES (N'efd5f86f-c5db-483e-ce36-08d7c41d8cc9', N'00140305F2C4', N'Elegoo Uno')
-INSERT INTO [dbo].[Devices] ([Id], [DeviceAddress], [DeviceName]) VALUES (N'23495bce-609d-455e-9ad6-08d7c421f959', N'00140305EEFA', N'Arduino Uno')
+INSERT INTO [dbo].[DevicesInfo] ([Id], [DeviceAddress], [DeviceName]) VALUES (N'efd5f86f-c5db-483e-ce36-08d7c41d8cc9', N'00140305F2C4', N'Elegoo Uno')
+INSERT INTO [dbo].[DevicesInfo] ([Id], [DeviceAddress], [DeviceName]) VALUES (N'23495bce-609d-455e-9ad6-08d7c421f959', N'00140305EEFA', N'Arduino Uno')
